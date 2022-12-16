b75722afa6db20383fdf65f7b6f42a0d14a253a5
glauber-cm <54870054+glauber-cm@users.noreply.github.com>
2022-12-15 21:53:31 -0300

    Desafio de Codigo Basico Csharp

---
+++ EncaixaOuNao/Program.cs
@@ -0,0 +1,19 @@
+﻿        int qt = int.Parse(Console.ReadLine());
+        string[] v = new string[2];
+
+        for (int i = 0; i < qt; i++ )
+        {
+            
+            v = Console.ReadLine().Split(' ');    
+
+            string a = v[0];
+            string b = v[1];
+
+            if (  b[1] > a[0] )
+                Console.WriteLine("nao encaixa");
+            else if (a.EndsWith(b))
+                Console.WriteLine("encaixa");
+            else
+                Console.WriteLine("nao encaixa");
+
+        }
